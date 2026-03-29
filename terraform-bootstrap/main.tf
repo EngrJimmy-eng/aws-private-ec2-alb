e29d82cb237d1175e5887f20047a1d08ea633dd2
Engr.Jimmy <ndumeleikenna@gmail.com>
2026-03-29 14:42:03 +0100

    Update main.tf

--- terraform-bootstrap/main.tf
+++ terraform-bootstrap/main.tf
@@ -22,3 +22,20 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
     }
   }
 }
+
+resource "aws_dynamodb_table" "tf_lock" {
+  name         = "terraform-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name = "Terraform Lock Table"
+  }
+}
+
+
