ae04e3790778dfe89185306640ea43b1b82bc019
Engr.Jimmy <ndumeleikenna@gmail.com>
2026-03-29 09:34:28 +0100

    Update main.tf

--- terraform-bootstrap/main.tf
+++ terraform-bootstrap/main.tf
@@ -1,7 +1,24 @@
+# S3 bucket
 resource "aws_s3_bucket" "tf_state" {
   bucket = "jimmytech-terraform-state-12345"
+}
 
-  tags = {
-    Name = "Terraform State Bucket"
+# Versioning (PUT YOUR CODE HERE)
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Encryption
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
   }
 }
