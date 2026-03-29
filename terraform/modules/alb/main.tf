fcc63b520b7ea805a83f9c9d5bcef8b77e28d3c0
Engr.Jimmy <ndumeleikenna@gmail.com>
2026-03-29 22:17:04 +0100

    Create main.tf

---
+++ terraform/modules/alb/main.tf
@@ -0,0 +1,80 @@
+
+# Security Group for ALB
+
+resource "aws_security_group" "alb_sg" {
+  name        = "${var.name}-sg"
+  description = "Allow HTTP inbound traffic"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "HTTP from internet"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.name}-sg"
+  }
+}
+
+
+# Application Load Balancer
+
+resource "aws_lb" "alb" {
+  name               = var.name
+  load_balancer_type = "application"
+  subnets            = var.public_subnets
+  security_groups    = [aws_security_group.alb_sg.id]
+
+  tags = {
+    Name = var.name
+  }
+}
+
+
+# Target Group
+
+resource "aws_lb_target_group" "tg" {
+  name     = "${var.name}-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+
+  health_check {
+    enabled             = true
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200"
+  }
+
+  tags = {
+    Name = "${var.name}-tg"
+  }
+}
+
+
+# Listener (HTTP)
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
