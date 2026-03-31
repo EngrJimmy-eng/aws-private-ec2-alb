fd40e7cb1e91a0e869887f7fd2a48e679f180e61
Engr.Jimmy <ndumeleikenna@gmail.com>
2026-04-01 00:32:00 +0100

    Update data.tf

--- terraform/environments/prod/data.tf
+++ terraform/environments/prod/data.tf
@@ -29,3 +29,14 @@ data "aws_subnets" "private" {
     Name = "Private-*"
   }
 }
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  owners = ["099720109477"] # Canonical (official Ubuntu)
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
