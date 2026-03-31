21e19d88ffab007b21d1f9652092d94ee178ba91
Engr.Jimmy <ndumeleikenna@gmail.com>
2026-04-01 00:09:39 +0100

    Update main.tf

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "app" {
   ami                    = var.ami
   instance_type          = var.instance_type
   subnet_id              = var.private_subnets[0]
-  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  vpc_security_group_ids = [data.aws_security_group.ec2_sg.id]
   associate_public_ip_address = false
 
   user_data = <<-EOF
