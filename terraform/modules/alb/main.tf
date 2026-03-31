56fda70ecb3adbc36f8b198a81c195b5429f3df4
Engr.Jimmy <ndumeleikenna@gmail.com>
2026-03-31 23:31:11 +0100

    Update main.tf

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -1,40 +1,20 @@
-resource "aws_security_group" "alb_sg" {
-  # Use the existing name
-  name        = "app-alb-sg"
-  description = "Allow HTTP inbound traffic"
-  vpc_id      = var.vpc_id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  lifecycle {
-    prevent_destroy = true
-  }
-
-  tags = {
-    Name = "app-alb-sg"
-  }
-}
 ########################################
-# Random ID for Target Group
+# EXISTING ALB SECURITY GROUP (DATA SOURCE)
+########################################
+data "aws_security_group" "alb_sg" {
+  name   = "app-alb-sg"
+  vpc_id = var.vpc_id
+}
+
+########################################
+# Random ID for Target Group (to avoid duplicates)
 ########################################
 resource "random_id" "tg" {
-  byte_length = 2  # creates 4 hex chars, unique suffix
+  byte_length = 2
 }
 
 ########################################
-# Target Group
+# Target Group (NEW - Terraform managed)
 ########################################
 resource "aws_lb_target_group" "tg" {
   name     = "app-alb-tg-${random_id.tg.hex}"
@@ -45,27 +25,11 @@ resource "aws_lb_target_group" "tg" {
   health_check {
     path = "/"
   }
-
-  lifecycle {
-    prevent_destroy = true
-  }
 }
 
 ########################################
-# Application Load Balancer
+# EXISTING ALB (DATA SOURCE)
 ########################################
-resource "aws_lb" "alb" {
-  name               = "app-alb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.alb_sg.id]
-  subnets            = var.public_subnets
-
-  lifecycle {
-    prevent_destroy = true
-  }
-
-  tags = {
-    Name = "app-alb"
-  }
+data "aws_lb" "alb" {
+  name = "app-alb"
 }
