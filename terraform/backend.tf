85a430c0a60723ec0bbaaa1ff91e60de6340afc8
Engr.Jimmy <ndumeleikenna@gmail.com>
2026-04-01 11:37:32 +0100

    Delete terraform/backend.tf

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "jimmytech-terraform-state-12345"
-    key            = "prod/terraform.tfstate"
-    region         = "eu-west-1"
-    dynamodb_table = "terraform-lock"
-    encrypt        = true
-  }
-}
