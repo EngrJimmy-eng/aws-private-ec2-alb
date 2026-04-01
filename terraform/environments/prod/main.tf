4ae74025bb19535e7473ba2dd24bb580ffb6fb30
Engr.Jimmy <ndumeleikenna@gmail.com>
2026-04-01 11:32:27 +0100

    Update main.tf

--- terraform/environments/prod/main.tf
+++ terraform/environments/prod/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket         = "ikenna-terraform-state"   # 🔁 choose a unique name
+    key            = "prod/terraform.tfstate"
+    region         = "eu-west-1"
+    dynamodb_table = "terraform-locks"
+  }
+}
+
+
 module "alb" {
   source = "../../modules/alb"
 
