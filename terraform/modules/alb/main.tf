62771eb51ac6ddf998d3695f3b03f65dd12c0989
Engr.Jimmy <ndumeleikenna@gmail.com>
2026-04-01 07:35:23 +0100

    Update main.tf

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -33,3 +33,20 @@ resource "aws_lb_target_group" "tg" {
 data "aws_lb" "alb" {
   name = "app-alb"
 }
+
+########################################
+# ALB LISTENER (HTTP → TARGET GROUP)
+########################################
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = data.aws_lb.alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
+
+
+
